b1673031bc6004f8e40bd298aee09ece4b9c92e3
jkent13 <josh-kent@hotmail.com>
2015-05-25 12:55:51 +1000

    Integration of all new UI classes into the main SSS project
    Fully up-to-date code branch from all team members 
    25/05/15

--- SSS/src/test/TestPos.java
+++ SSS/src/test/TestPos.java
@@ -8,33 +8,6 @@
 
 package test;
 
-import java.awt.Font;
-import java.awt.Frame;
-import java.awt.GridLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.math.BigDecimal;
-import java.sql.SQLException;
-
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
-import javax.swing.border.TitledBorder;
-import javax.swing.table.TableColumnModel;
-
-import sss.domain.LineItemTableModel;
-import sss.domain.Register;
 import sss.ui.PosFrame;
 
 
