ee1fe16ada3fa7ad9ede97f7abff58054a2634d5
amayer06 <katbat_@hotmail.com>
2015-08-26 15:57:55 +1000

    nothing's actually changed

--- SSS/src/sss/ui/ReportsMenuFrame.java
+++ SSS/src/sss/ui/ReportsMenuFrame.java
@@ -5,6 +5,7 @@
  */
 
 package sss.ui;
+import java.awt.BorderLayout;
 import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -55,7 +56,8 @@ public class ReportsMenuFrame extends JFrame {
 		//--------------------Section Panels--------------------
 
 		JPanel topPanel = new JPanel();
-		topPanel.setLayout(new GridLayout(1,1,10,10));
+		topPanel.setLayout(new BorderLayout());
+//		topPanel.setLayout(new GridLayout(1,1,10,10));
 		fullScreenPanel.add(topPanel);
 
 		JLabel reportsTextLabel = new JLabel(new ImageIcon(reportsMenuLogoUrl));
